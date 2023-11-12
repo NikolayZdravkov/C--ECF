38a960462aac7d475d5ea20d4f37d91ece1cec57
Nikolay Zdravkov <90557280+NikolayZdravkov@users.noreply.github.com>
2023-11-12 22:30:46 +0200

    Add files via upload

---
+++ LINQ/MusicHub/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicHub.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=DESKTOP-30287UE\SQLEXPRESS;Database=MusicHub;Integrated Security=True";
+    }
+}
